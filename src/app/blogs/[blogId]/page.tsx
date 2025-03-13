d97a91494b93ca4d6bf9c5ac6456ccd831ff049b
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2025-03-13 13:04:10 +0600

    dynamic metadata

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,27 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+  return {
+    title: blog.title,
+  };
+}
 
 const BlogIdpage = async ({ params }: { params: { blogId: string } }) => {
   const { blogId } = await params;
